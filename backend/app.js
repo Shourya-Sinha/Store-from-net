28f7bd3a324ad4c0e7e2cd4de6413150a9e2824c
Shourya-Sinha <shouryasinha.c@gmail.com>
2024-07-02 15:57:36 +0700

    Changing cors rules

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,9 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(cors({
+    origin: 'https://store-from-net.vercel.app'
+  }));
 app.options('*', cors());
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({extended: true }));
